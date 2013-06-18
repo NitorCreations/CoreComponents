19b1f0a3712a5b75f96e3ee3071a6528d0ea78e1
niom <marko.pukari@punchwolf.com>
2013-06-18 11:39:58 +0300

    Reproduce Issue: #3

---
+++ junit-runners/src/test/java/com/nitorcreations/junit/runners/examples/FailingNestedRunnerTest.java
@@ -0,0 +1,45 @@
+package com.nitorcreations.junit.runners.examples;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.internal.builders.JUnit3Builder;
+import org.junit.matchers.JUnitMatchers;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+
+import com.nitorcreations.junit.runners.NestedRunner;
+
+public class FailingNestedRunnerTest {
+
+	@Test
+	public void ShouldFail() {
+		Result result = JUnitCore.runClasses(Parent.class);
+		assertThat(result.getFailureCount(), is(1));
+		assertTrue(result.getFailures().get(0).getException().getClass() == RuntimeException.class);
+	}
+
+	@RunWith(NestedRunner.class)
+	public static class Parent {
+		public class Failing {
+			public Failing() {
+				throw new RuntimeException();
+			}
+
+			@Before
+			public void setup() {
+			}
+
+			@Test
+			public void isNeverExecuted() {
+			}
+
+		}
+	}
+
+}
