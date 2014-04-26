2332662d3e3a8509810693c0a814870cb805c54b
Pasi Niemi <pasi.niemi@iki.fi>
2014-04-26 12:42:45 +0300

    Add timestamp

--- deployer/src/main/java/com/nitorcreations/deployer/messages/AbstractMessage.java
+++ deployer/src/main/java/com/nitorcreations/deployer/messages/AbstractMessage.java
@@ -3,6 +3,7 @@ package com.nitorcreations.deployer.messages;
 import com.google.gson.Gson;
 
 public class AbstractMessage {
+	public long timestamp = System.currentTimeMillis();
 	public String toString() {
 		return new Gson().toJson(this);
 	}
