a1a273c67b342ace18e35578df6eae8a1d0a3c96
Pasi Niemi <pasi.niemi@iki.fi>
2014-04-26 12:43:07 +0300

    Exec to get rid of unnecessary  bash process

--- deployer/deploy.sh
+++ deployer/deploy.sh
@@ -5,4 +5,4 @@ if ! [ -r $JAVA_HOME/lib/tools.jar ]; then
    exit 1
 fi
 
-$JAVA_HOME/bin/java -cp $JAVA_HOME/lib/tools.jar:target/deployer-1.0-SNAPSHOT-jar-with-dependencies.jar com.nitorcreations.deployer.Main com.nitorcreations:liipy-jersey:jar:uber:1.0-SNAPSHOT
+exec $JAVA_HOME/bin/java -cp $JAVA_HOME/lib/tools.jar:target/deployer-1.0-SNAPSHOT-jar-with-dependencies.jar com.nitorcreations.deployer.Main com.nitorcreations:liipy-jersey:jar:uber:1.0-SNAPSHOT
