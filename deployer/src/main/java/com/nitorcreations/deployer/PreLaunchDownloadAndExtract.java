a052a44c166902bb02d443c24cbe2498e7390a35
Pasi Niemi <pasi.niemi@nitorcreations.com>
2014-08-01 09:24:33 +0300

    Download and extract work

--- deployer/src/main/java/com/nitorcreations/deployer/PreLaunchDownloadAndExtract.java
+++ deployer/src/main/java/com/nitorcreations/deployer/PreLaunchDownloadAndExtract.java
@@ -9,9 +9,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -19,6 +24,7 @@ import java.util.Set;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.velocity.runtime.parser.node.GetExecutor;
 
 import static com.nitorcreations.deployer.PropertyKeys.*;
 
@@ -37,28 +43,21 @@ public class PreLaunchDownloadAndExtract {
 		perms.put(0400, PosixFilePermission.OWNER_READ);
 	}
 
-	private void extractTar(InputStream in, File destFolder) throws Exception {
+	private void extractTar(InputStream in, File destFolder, Set<PathMatcher> skipMatchers, Set<PathMatcher> filterMatchers) throws Exception {
 		// Create a TarInputStream
 		TarArchiveInputStream tis = new TarArchiveInputStream(new BufferedInputStream(in));
 		TarArchiveEntry entry;
 
 		while((entry = (TarArchiveEntry) tis.getNextEntry()) != null) {
-			int count;
-			byte data[] = new byte[2048];
 			FileOutputStream fos = new FileOutputStream(new File(destFolder, entry.getName()));
-			BufferedOutputStream dest = new BufferedOutputStream(fos);
-			Set<PosixFilePermission> perms = getPermissions(entry.getMode());
-
-			while((count = tis.read(data)) != -1) {
-				dest.write(data, 0, count);
+			if (!globMatches(entry.getName(), skipMatchers)) {
+				BufferedOutputStream dest = new BufferedOutputStream(fos);
+				Set<PosixFilePermission> perms = getPermissions(entry.getMode());
 			}
-
-			dest.flush();
-			dest.close();
 		}
-
 		tis.close();
 	}
+
 	public void execute(Properties properties) {
 		Map<String, String> replaceTokens = new HashMap<>();
 		for (Entry<Object,Object> nextEntry : properties.entrySet()) {
@@ -68,6 +67,10 @@ public class PreLaunchDownloadAndExtract {
 		if (downloadUrl("", properties, replaceTokens)) {
 			int i = 1;
 			while (downloadUrl("" + i, properties, replaceTokens)) {}
+		}
+		if (downloadArtifact("", properties, replaceTokens)) {
+			int i = 1;
+			while (downloadArtifact("" + i, properties, replaceTokens)) {}
 		}
 	}
 	
@@ -83,20 +86,61 @@ public class PreLaunchDownloadAndExtract {
 			URLConnection conn = new URL(url).openConnection();
 			FileUtil.copy(conn.getInputStream(), target);
 			if (lcFileName.endsWith("tar.gz") || lcFileName.endsWith(".tgz")) {
-				extractTar(new FileInputStream(target), new File(root));
+				extractTar(new FileInputStream(target), new File(root),
+						getGlobMatchers(properties.getProperty(PROPERTY_KEY_PREFIX_EXTRACT_SKIP_GLOB +propertySuffix)),
+						getGlobMatchers(properties.getProperty(PROPERTY_KEY_PREFIX_EXTRACT_INTERPOLATE_GLOB +propertySuffix)));
+				target.delete();
+			} else if (lcFileName.endsWith(".zip") || lcFileName.endsWith(".jar")) {
+				extractZip(new FileInputStream(target), new File(root),
+						getGlobMatchers(properties.getProperty(PROPERTY_KEY_PREFIX_EXTRACT_SKIP_GLOB +propertySuffix)),
+						getGlobMatchers(properties.getProperty(PROPERTY_KEY_PREFIX_EXTRACT_INTERPOLATE_GLOB +propertySuffix)));
+				target.delete();
+			} else if (lcFileName.endsWith(".cpio")) {
+				extractCpio(new FileInputStream(target), new File(root),
+						getGlobMatchers(properties.getProperty(PROPERTY_KEY_PREFIX_EXTRACT_SKIP_GLOB +propertySuffix)),
+						getGlobMatchers(properties.getProperty(PROPERTY_KEY_PREFIX_EXTRACT_INTERPOLATE_GLOB +propertySuffix)));
+				target.delete();
+			} else {
+				target.renameTo(new File(new File(root), fileName));
 			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return true;
 	}
 	
+	private void extractCpio(FileInputStream fileInputStream, File file,
+			Set<PathMatcher> globMatchers, Set<PathMatcher> globMatchers2) {
+		// TODO Auto-generated method stub
+		
+	}
+	private void extractZip(FileInputStream fileInputStream, File file,
+			Set<PathMatcher> globMatchers, Set<PathMatcher> globMatchers2) {
+		// TODO Auto-generated method stub
+		
+	}
 	private boolean downloadArtifact(String propertySuffix, Properties properties, Map<String, String> replaceTokens) {
 		String artifact = properties.getProperty(PROPERTY_KEY_PREFIX_DOWNLOAD_ARTIFACT);
 		if (artifact == null) return false;
 		return true;
 	}
 	
+	private boolean globMatches(String path, Set<PathMatcher> matchers) {
+		for (PathMatcher next : matchers) {
+			if (next.matches(Paths.get(path))) return true;
+		}
+		return false;
+	}
+	private Set<PathMatcher> getGlobMatchers(String expressions) {
+		Set<PathMatcher> matchers = new LinkedHashSet<>();
+		if (expressions == null || expressions.isEmpty()) return matchers;
+		FileSystem def = FileSystems.getDefault();
+		for (String next : expressions.split("|")) {
+			matchers.add(def.getPathMatcher("glob:"  + expressions));
+		}
+		return matchers;
+	}
 	
 	public Set<PosixFilePermission> getPermissions(int mode) {
 		Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
--- deployer/src/main/java/com/nitorcreations/deployer/PropertyKeys.java
+++ deployer/src/main/java/com/nitorcreations/deployer/PropertyKeys.java
@@ -6,6 +6,7 @@ public interface PropertyKeys {
 	public static final String PROPERTY_KEY_PREFIX_DOWNLOAD_ARTIFACT        = "deployer.download.artifact";
 	public static final String PROPERTY_KEY_PREFIX_EXTRACT_INTERPOLATE_GLOB = "deployer.extract.interpolate.glob";
 	public static final String PROPERTY_KEY_PREFIX_EXTRACT_ROOT             = "deployer.extract.root";
+	public static final String PROPERTY_KEY_PREFIX_EXTRACT_SKIP_GLOB        = "deployer.extract.root";
 	public static final String PROPERTY_KEY_LOCAL_REPOSITORY                = "deployer.local.repository";
 	public static final String PROPERTY_KEY_REMOTE_REPOSITORY               = "deployer.remote.repository";
 	public static final String PROPERTY_KEY_PREFIX_JAVA_ARGS                = "deployer.java.arg";
