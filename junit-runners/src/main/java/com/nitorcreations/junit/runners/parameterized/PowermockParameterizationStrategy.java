586286a8a526c65155ce79b0a0be55a262296e58
Jonas Berlin <jonas.berlin@nitorcreations.com>
2015-10-22 14:58:20 +0300

    Add javadoc to clarify the purpose and implementation of the class

--- junit-runners/src/main/java/com/nitorcreations/junit/runners/parameterized/PowermockParameterizationStrategy.java
+++ junit-runners/src/main/java/com/nitorcreations/junit/runners/parameterized/PowermockParameterizationStrategy.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Nitor Creations Oy
+ * Copyright 2014-2015 Nitor Creations Oy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,37 @@ import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.GeneratorAdapter;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 
+/**
+ * Explicit support for PowerMock.
+ * <p>
+ * PowerMock loads test classes using javassist's ClassPool mechanism, which
+ * will not see the classes we inject into the context classloader. So instead
+ * we amend PowerMock's classpath with a ByteArrayClassPath that loads the
+ * testclass in question, only.
+ * <p>
+ * To achieve this:
+ * <ul>
+ * <li>Create a helper class that implements PowerMock's @
+ * {@link org.powermock.core.classloader.ClassPathAdjuster.ClassPathAdjuster}
+ * interface.</li>
+ * <li>Store both the testclass bytecode (as an byte array) and the constructor
+ * arguments in public static fields in the helper class.</li>
+ * <li>Tell PowerMock to amend its classpath to include our helper class using
+ * the @{@link org.powermock.core.classloader.annotations.UseClassPathAdjuster}
+ * annotation</li>
+ * <li>Also exempt the package of the helper class using @
+ * {@link PowerMockIgnore} on the test class.</li>
+ * <li>When PowerMock calls our helper class, we add a new @
+ * {@link javassist.ByteArrayClassPath} instance initialized to load the test
+ * class from the byte array.</li>
+ * <li>When PowerMock has loaded our test class and constructs it, fetch the
+ * constructor arguments for the superclass from the public static field in the
+ * helper class.</li>
+ * </ul>
+ * <p>
+ * <i>The ClassPathAdjuster interface and the UseClassPathAdjuster annotation
+ * were contributed to PowerMock in order to unlock this stunt. :)</i>
+ */
 public class PowermockParameterizationStrategy implements
 		ParameterizationStrategy {
 
@@ -49,7 +80,8 @@ public class PowermockParameterizationStrategy implements
 
 	private static final String CLASSPATH_ADJUSTER_CLASS_INTERNALNAME = "org/powermock/core/classloader/ClassPathAdjuster";
 	private static final String USE_CLASSPATH_ADJUSTER_CLASS_DESCRIPTOR = "Lorg/powermock/core/classloader/annotations/UseClassPathAdjuster;";
-	private static final String CLASSPATH_ADJUSTER_CLASSNAME = CLASSPATH_ADJUSTER_CLASS_INTERNALNAME.replace('/','.');
+	private static final String CLASSPATH_ADJUSTER_CLASSNAME = CLASSPATH_ADJUSTER_CLASS_INTERNALNAME
+			.replace('/', '.');
 
 	private static String getPowerMockHelperClassRaw(String nameRaw) {
 		String powerMockHelperClassRaw = PACKAGE_FOR_POWERMOCK_AVOIDANCE_RAW
@@ -60,12 +92,13 @@ public class PowermockParameterizationStrategy implements
 
 	private String nameRaw;
 	private String powerMockHelperClassRaw;
-	
+
 	public PowermockParameterizationStrategy() {
 		try {
 			Class.forName(CLASSPATH_ADJUSTER_CLASSNAME);
-		} catch(ClassNotFoundException e) {
-			throw new ParameterizationStrategyNotAvailableException("Your version of PowerMock is too old - version 1.5.5 or later required");
+		} catch (ClassNotFoundException e) {
+			throw new ParameterizationStrategyNotAvailableException(
+					"Your version of PowerMock is too old - version 1.5.5 or later required");
 		}
 	}
 
@@ -98,20 +131,25 @@ public class PowermockParameterizationStrategy implements
 			Object[] constructorArgs) {
 		ClassWriter cw = new ClassWriter(0);
 
+		// public class PowerMockHelperClass implements ClassPathAdjuster {
 		cw.visit(V1_5, ACC_PUBLIC + ACC_SUPER, powerMockHelperClassRaw, null,
 				"java/lang/Object",
 				new String[] { CLASSPATH_ADJUSTER_CLASS_INTERNALNAME });
 
+		// public static byte[] testClassBytes;
 		cw.visitField(ACC_PUBLIC + ACC_STATIC, FIELD_TEST_CLASS_BYTE_ARR, "[B",
 				null, null).visitEnd();
 
+		// public static Object[] constructorArgs;
 		cw.visitField(ACC_PUBLIC + ACC_STATIC, FIELD_CONSTRUCTOR_ARGS,
 				"[Ljava/lang/Object;", null, null).visitEnd();
 
+		// public PowerMockHelperClass() {
 		{
 			MethodVisitor mv;
 			mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
 			mv.visitCode();
+			// super();
 			mv.visitVarInsn(ALOAD, 0);
 			mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>",
 					"()V");
@@ -119,11 +157,15 @@ public class PowermockParameterizationStrategy implements
 			mv.visitMaxs(1, 1);
 			mv.visitEnd();
 		}
+		// @Override
+		// public void adjustClassPath(ClassPool pool) {
 		{
 			MethodVisitor mv;
 			mv = cw.visitMethod(ACC_PUBLIC, "adjustClassPath",
 					"(Ljavassist/ClassPool;)V", null, null);
 			mv.visitCode();
+			// pool.appendClassPath(new ByteArrayClassPath("TestClass_0",
+			// testClassBytes));
 			mv.visitVarInsn(ALOAD, 1);
 			mv.visitTypeInsn(NEW, "javassist/ByteArrayClassPath");
 			mv.visitInsn(DUP);
