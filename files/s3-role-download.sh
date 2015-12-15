9bd598fe50a74e2d5bf602f200b43f18a395bfe1
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-12-15 18:11:54 +0200

    Add s3 download script

---
+++ files/s3-role-download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILE=$3
+BUCKET=$2
+ROLE=$1
+CONTENT_TYPE="application/octet-stream"
+DATE=$(date -R)
+RESOURCE="/${BUCKET}/${FILE}"
+TMP=$(mktemp)
+curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/${ROLE} | egrep ^[[:space:]]*\" | sed 's/[^\"]*\"\([^\"]*\)\".:.\"\([^\"]*\).*/\1=\2/g' > $TMP
+source $TMP
+rm -f $TMP
+SIGNSTR="GET\n\n${CONTENT_TYPE}\n${DATE}\nx-amz-security-token:${Token}\n${RESOURCE}"
+SIGNATURE=$(echo -en ${SIGNSTR} | openssl sha1 -hmac ${SecretAccessKey} -binary | base64)
+curl -s -o $(basename ${FILE}) -X GET -H "Host: ${BUCKET}.s3.amazonaws.com" -H "Date: ${DATE}" -H "Content-Type: ${CONTENT_TYPE}" -H "Authorization: AWS ${AccessKeyId}:${SIGNATURE}" -H "x-amz-security-token: ${Token}" https://${BUCKET}.s3.amazonaws.com/${FILE}
